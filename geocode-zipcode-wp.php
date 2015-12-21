27f0de46399cca002e387a1748b272aede8307c0
Ed Gifford <egifford@blueblazeassociates.com>
2015-12-21 11:48:23 -0500

    Bumping version to 0.1.1

--- geocode-zipcode-wp.php
+++ geocode-zipcode-wp.php
@@ -4,7 +4,7 @@ Plugin Name: Blue Blaze Postal Code Geocoder
 Description: Provides a postal code geocoder utility.
 Author:      Blue Blaze Associates
 Author URI:  http://www.blueblazeassociates.com
-Version:     0.1.0
+Version:     0.1.1
 */
 
 if ( ! class_exists( '\BlueBlazeAssociates\Geocoding\PostalCodes\WordPress\Geocoders\USPostalCodeGeocoder' ) ) {
